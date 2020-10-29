de5a3dfc3aa9c047daf25379b12197d20150284b
subki <tajudinsubki@gmail.com>
2020-10-30 02:10:13 +0700

    benerin session

--- application/config/config.php
+++ application/config/config.php
@@ -387,14 +387,20 @@ $config['encryption_key'] = 'pengkode';
 //$config['sess_time_to_update'] = 300;
 //$config['sess_regenerate_destroy'] = FALSE;
 
+//$config['sess_driver'] = 'database';
+//$config['sess_cookie_name'] = 'ci_session';
+//$config['sess_expiration'] = 7200;
+//$config['sess_save_path'] = 'ci_sessions';
+//$config['sess_match_ip'] = FALSE;
+//$config['sess_time_to_update'] = 300;
+//$config['sess_regenerate_destroy'] = FALSE;
 $config['sess_driver'] = 'database';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = 'ci_sessions';
-$config['sess_match_ip'] = FALSE;
+$config['sess_cookie_name'] = 'newname_session';
+$config['sess_expiration'] = 72000;
+$config['sess_save_path'] ='newname_sessions';
+$config['sess_match_ip'] = TRUE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
-
 /*
 |--------------------------------------------------------------------------
 | Cookie Related Variables
